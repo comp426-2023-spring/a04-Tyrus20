2874696dab1a927e2aaf12357f0a32eeb6bcab10
Tyrus <tyrus@email.unc.edu>
2023-05-09 10:04:08 -0400

    test1

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ import minimist from 'minimist';
 import express from 'express';
 import {rps} from './lib/rpsls.js';
 import {rpsls} from './lib/rpsls.js';
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+const app = express();
