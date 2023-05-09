78a461180a4419b9327d8a1e4e8dbcc438e6a2d2
Tyrus <tyrus@email.unc.edu>
2023-05-09 10:44:53 -0400

    finish express setup

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get('/app', (req, res) => {
     res.status(200).send("200 OK");
 });
