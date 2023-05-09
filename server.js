9b5b6eb8a2f8609ebf232bb35b8580c46b48ec8f
Tyrus <tyrus@email.unc.edu>
2023-05-09 10:16:44 -0400

    added default API endpoint

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ import {rpsls} from './lib/rpsls.js';
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 const app = express();
+
+app.length('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND');
+})
